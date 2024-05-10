60c69c5aaae3b8c0632cdb589ae66fbbccfa248c
gaviny82 <38808970+gaviny82@users.noreply.github.com>
2024-05-10 22:24:26 +0100

    Update run benchmark script

--- run-benchmark.ps1
+++ run-benchmark.ps1
@@ -1,5 +1,5 @@
 # Run benchmarks for MinecraftToolkit.Nbt in the CLI provided by BenchmarkDotNet
-$benchmarkProjectPath = "./Benchmarks/MinecraftToolkit.Nbt.Benchmark"
+$benchmarkProjectPath = "./benchmarks/MinecraftToolkit.Nbt.Benchmark"
 $runtimeArgs = $args -join ' '
 $cmd = "dotnet run --project $benchmarkProjectPath --configuration Release -- $runtimeArgs"
 Invoke-Expression $cmd
