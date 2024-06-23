a892a3d9f1d31dfaed03af04b9b8a3d2e1a14978
gaviny82 <38808970+gaviny82@users.noreply.github.com>
2024-06-24 01:55:11 +0800

    Add NbtWriter sample

--- samples/LevelFile/Program.cs
+++ samples/LevelFile/Program.cs
@@ -4,4 +4,10 @@ string levelPath = Console.ReadLine() ?? throw new InvalidDataException();
 using var fileStream = File.OpenRead(levelPath);
 var nbtReader = new NbtReader(fileStream, NbtCompression.GZip);
 var tagCompound = nbtReader.ReadRootTag();
+
+using FileStream fs = File.Open(Path.Combine(Environment.CurrentDirectory, "level_copy.dat"), FileMode.Create);
+NbtWriter nbtWriter = new NbtWriter(fs, NbtCompression.GZip);
+nbtWriter.WriteRootTag(tagCompound);
+nbtWriter.Dispose();
+
 Console.ReadKey();
