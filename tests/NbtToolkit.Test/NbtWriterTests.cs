dabb098a7ab99c21639c6b440635075882161921
gaviny82 <38808970+gaviny82@users.noreply.github.com>
2024-07-20 23:08:26 +0800

    Add tests for NbtWriter for simple TagList

--- tests/NbtToolkit.Test/NbtWriterTests.cs
+++ tests/NbtToolkit.Test/NbtWriterTests.cs
@@ -1,6 +1,196 @@
-﻿namespace NbtToolkit.Test;
+﻿using NbtToolkit.Binary;
+
+namespace NbtToolkit.Test;
 
 public class NbtWriterTests
 {
+    [Fact]
+    public void WriteTagList_Byte_WriteCorrectBytes()
+    {
+        // Arrange
+        byte[] bytes = NbtBinaryTestCases.TagList_Byte_Bytes;
+        sbyte[] values = NbtBinaryTestCases.TagList_Byte_Values;
 
+        using MemoryStream stream = new();
+        using NbtBinaryWriter writer = new(stream);
+
+        TagList<sbyte> list = [.. values];
+
+        // Act
+        list.WriteBinaryPayload(writer);
+
+        // Assert
+        Assert.Equal(bytes, stream.ToArray());
+    }
+
+    [Fact]
+    public void WriteTagList_Short_WriteCorrectBytes()
+    {
+        // Arrange
+        byte[] bytes = NbtBinaryTestCases.TagList_Short_Bytes;
+        short[] values = NbtBinaryTestCases.TagList_Short_Values;
+
+        using MemoryStream stream = new();
+        using NbtBinaryWriter writer = new(stream);
+
+        TagList<short> list = [.. values];
+
+        // Act
+        list.WriteBinaryPayload(writer);
+
+        // Assert
+        Assert.Equal(bytes, stream.ToArray());
+    }
+
+    [Fact]
+    public void WriteTagList_Int_WriteCorrectBytes()
+    {
+        // Arrange
+        byte[] bytes = NbtBinaryTestCases.TagList_Int_Bytes;
+        int[] values = NbtBinaryTestCases.TagList_Int_Values;
+
+        using MemoryStream stream = new();
+        using NbtBinaryWriter writer = new(stream);
+
+        TagList<int> list = [.. values];
+
+        // Act
+        list.WriteBinaryPayload(writer);
+
+        // Assert
+        Assert.Equal(bytes, stream.ToArray());
+    }
+
+    [Fact]
+    public void WriteTagList_Long_WriteCorrectBytes()
+    {
+        // Arrange
+        byte[] bytes = NbtBinaryTestCases.TagList_Long_Bytes;
+        long[] values = NbtBinaryTestCases.TagList_Long_Values;
+
+        using MemoryStream stream = new();
+        using NbtBinaryWriter writer = new(stream);
+
+        TagList<long> list = [.. values];
+
+        // Act
+        list.WriteBinaryPayload(writer);
+
+        // Assert
+        Assert.Equal(bytes, stream.ToArray());
+    }
+
+    [Fact]
+    public void WriteTagList_Float_WriteCorrectBytes()
+    {
+        // Arrange
+        byte[] bytes = NbtBinaryTestCases.TagList_Float_Bytes;
+        float[] values = NbtBinaryTestCases.TagList_Float_Values;
+
+        using MemoryStream stream = new();
+        using NbtBinaryWriter writer = new(stream);
+
+        TagList<float> list = [.. values];
+
+        // Act
+        list.WriteBinaryPayload(writer);
+
+        // Assert
+        Assert.Equal(bytes, stream.ToArray());
+    }
+
+    [Fact]
+    public void WriteTagList_Double_WriteCorrectBytes()
+    {
+        // Arrange
+        byte[] bytes = NbtBinaryTestCases.TagList_Double_Bytes;
+        double[] values = NbtBinaryTestCases.TagList_Double_Values;
+
+        using MemoryStream stream = new();
+        using NbtBinaryWriter writer = new(stream);
+
+        TagList<double> list = [.. values];
+
+        // Act
+        list.WriteBinaryPayload(writer);
+
+        // Assert
+        Assert.Equal(bytes, stream.ToArray());
+    }
+
+    [Fact]
+    public void WriteTagList_String_WriteCorrectBytes()
+    {
+        // Arrange
+        byte[] bytes = NbtBinaryTestCases.TagList_String_Bytes;
+        string[] values = NbtBinaryTestCases.TagList_String_Values;
+
+        using MemoryStream stream = new();
+        using NbtBinaryWriter writer = new(stream);
+
+        TagList<string> list = [.. values];
+
+        // Act
+        list.WriteBinaryPayload(writer);
+
+        // Assert
+        Assert.Equal(bytes, stream.ToArray());
+    }
+
+    [Fact]
+    public void WriteTagList_ByteArray_WriteCorrectBytes()
+    {
+        // Arrange
+        byte[] bytes = NbtBinaryTestCases.TagList_ByteArray_Bytes;
+        sbyte[][] values = NbtBinaryTestCases.TagList_ByteArray_Values;
+
+        using MemoryStream stream = new();
+        using NbtBinaryWriter writer = new(stream);
+
+        TagList<sbyte[]> list = [.. values];
+
+        // Act
+        list.WriteBinaryPayload(writer);
+
+        // Assert
+        Assert.Equal(bytes, stream.ToArray());
+    }
+
+    [Fact]
+    public void WriteTagList_IntArray_WriteCorrectBytes()
+    {
+        // Arrange
+        byte[] bytes = NbtBinaryTestCases.TagList_IntArray_Bytes;
+        int[][] values = NbtBinaryTestCases.TagList_IntArray_Values;
+
+        using MemoryStream stream = new();
+        using NbtBinaryWriter writer = new(stream);
+
+        TagList<int[]> list = [.. values];
+
+        // Act
+        list.WriteBinaryPayload(writer);
+
+        // Assert
+        Assert.Equal(bytes, stream.ToArray());
+    }
+
+    [Fact]
+    public void WriteTagList_LongArray_WriteCorrectBytes()
+    {
+        // Arrange
+        byte[] bytes = NbtBinaryTestCases.TagList_LongArray_Bytes;
+        long[][] values = NbtBinaryTestCases.TagList_LongArray_Values;
+
+        using MemoryStream stream = new();
+        using NbtBinaryWriter writer = new(stream);
+
+        TagList<long[]> list = [.. values];
+
+        // Act
+        list.WriteBinaryPayload(writer);
+
+        // Assert
+        Assert.Equal(bytes, stream.ToArray());
+    }
 }
